da902605ad70cd8143ea0067d30b3160ffbdcf67
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2026-08-23 21:36:44 +0200

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <CssBaseline />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
